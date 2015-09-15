1ddbea36c131b0bfe1654c5ee9418b6fe5d519d1
Minh Le <minh.le@ucla.edu>
2015-09-15 03:46:23 -0700

    deleted swap files from vim

--- test.py
+++ test.py
@@ -3,9 +3,11 @@
 from circle_org import circle_org
 
 def main():
-    for x in range (0,11):
+    obj = set('')
+    for x in range (0,100):
         c = circle_org()
-        print (str(x) + ". " + str(c.fit_score))
-        c.save_image("test/test" + str(x) + ".jpg")
+        obj.add(c)
+        #print (str(x) + ". " + str(c.fit_score))
+        #c.save_image("test/test" + str(x) + ".jpg")
 if __name__ == '__main__':
     main()
